92777e747ec7ca0370a249506431e835b019a33d
Ada <obenedicta4@gmail.com>
2023-10-05 21:31:09 +0100

    airbnb

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,66 +1,49 @@
 #!/usr/bin/python3
 # web servers for the deployment of web_static
 # install nginx if not installed
+
+echo -e "\e[1;32m START\e[0m"
 sudo apt update
 sudo apt install -y nginx
 
-data="/data/"
-web_static="$data/web_static/"
-releases="$web_static/releases/"
-shared="$web_static/shared/"
-test_f="$releases/test/"
-index="$test_f/index.html"
-current="$web_static/current"
-nginx="/etc/nginx/sites-available/default"
-nginx_alias="location /hbnb_static/ { alias $current/; }"
+echo -e "\e[1;32m Packages updated\e[0m"
+echo
 
-# create folders
-if [ ! -d "$data" ]
-then
-    sudo mkdir -p $data
-    sudo chown -R ubuntu:ubuntu $data
-fi
+sudo ufw allow 'Nginx HTTP'
+echo -e "\e[1;32m Allow incoming NGINX HTTP connections\e[0m"
+echo
 
-if [ ! -d "$web_static" ]
-then
-    sudo mkdir -p $web_static
-    sudo chown -R ubuntu:ubuntu $web_static
-fi
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
+echo -e "\e[1;32m directories created"
+echo
 
-if [ ! -d "$releases" ]
-then
-    sudo mkdir -p $releases
-    sudo chown -R ubuntu:ubuntu $releases
-fi
-
-if [ ! -d "$shared" ]
-then
-    sudo mkdir -p $shared
-    sudo chown -R ubuntu:ubuntu $shared
-fi
-
-if [ ! -d "$test_f" ]
-then
-    sudo mkdir -p $test_f
-    sudo chown -R ubuntu:ubuntu $test_f
-fi
-
-# create fake html page
 echo "<html>
    <head><title>Testing My Domain</title></head>
    <body>
      <h1>Testing this page</h1>
    </body>
- </html>" | sudo tee $index > /dev/null
+ </html>" > /data/web_static/releases/test/index.html
+echo -e "\e[1;32m Test string added\e[0m"
+echo
 
-# create symbolic link
-if [-L "$current"]
+if [ -d "/data/web_static/current" ];
 then
-    sudo rm $current
-fi
+	echo "path /data/webstatic/current exists"
+	sudo rm -rf /data/web_static/current;
+fi;
+echo -e "\e[1;32m prevent overwritting\e[0m"
+echo
 
-sudo ln -s $test $current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-sudo sed -i "/^server {/a $nginx_alias" $nginx
+sudo chown -R ubuntu:ubuntu /data
+
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-available/default'
+echo -e "\e[1;32m Symbolic link created\e[0m"
+echo
 
 sudo service nginx restart
+echo -e "\e[1;32m restart NGINX\e[0m"
