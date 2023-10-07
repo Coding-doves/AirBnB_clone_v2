939d2ac7bce887621189a76994b3a349b8e50678
Ada <obenedicta4@gmail.com>
2023-10-08 00:03:18 +0100

    airbnb

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,52 +16,46 @@ def do_deploy():
         os.mkdir("versions")
 
     current_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+    outpt = "versions/web_static_{}{}{}{}{}{}.tgz".format(
             current_time.year, current_time.month,
             current_time.day, current_time.hour,
             current_time.minute, current_time.second
         )
     try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archive_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archive_size))
+        print("Packing web_static to {}".format(outpt))
+        local("tar -cvzf {} web_static".format(outpt))
+        archive_size = os.stat(outpt).st_size
+        print("web_static packed: {} -> {} Bytes".format(outpt, archive_size))
     except Exception as e:
-        print("Deploy1 failed")
         print(e)
-        output = None
+        outpt = None
 
-    return output
+    return outpt
 
 
 def do_deploy(archive_path):
-    """
-    sending static file to server
-
-    Arg: path_to_archive: this is the path to archive
-    """
+    """ comment """
     if not os.path.exists(archive_path):
         return False
 
     file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    path_to_folder = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
+    folder_n = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_n)
+    pas = False
 
     try:
         put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(path_to_folder))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, path_to_folder))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
         run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(path_to_folder, path_to_folder))
-        run("rm -rf {}web_static".format(path_to_folder))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(path_to_folder))
-        print("New version deployed!")
-        success = True
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        pas = True
     except Exception as e:
         print("deploy2 failed")
         print(e)
-    success = False
+        pas = False
 
-        return success'
+    return pas
