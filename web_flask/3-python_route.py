270d60b12a72809e314deac9fa0b165367589227
Ada <obenedicta4@gmail.com>
2023-10-20 09:18:36 +0100

    Web framework: flask

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+starts a Flask web application
+'''
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello():
+    ''' display “Hello HBNB!” '''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def horbolton():
+    ''' display “HBNB” '''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    ''' display “C” '''
+    return 'C {}'.format(text.replace('_', " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    ''' display “Python” '''
+    return 'Python {}'.format(text.replace('_', " "))
+
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
